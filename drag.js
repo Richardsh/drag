a70c0bb96f518dc915f3fe3b58f1b3a93cbd0c05
Richardsh <2223239184@qq.com>
2016-05-17 11:22:30 +0800

    success drag

--- drag.js
+++ drag.js
@@ -1,15 +1,20 @@
 // Javascript Document
 
-function(id){
+function drag(id){
     var obj = document.getElementById(id);
-    var disX=0,
-        disY=0;
+    var disX=0;
+    var disY=0;
     obj.onmousedown = function(ev){
         disX = ev.pageX - obj.offsetLeft;
         disY = ev.pageY - obj.offsetTop;
         document.onmousemove =function(ev){
-            obj.style.left = ev.pageX - ev.disX + "px";
-            obj.style.top = ev.pageY - ev.disY + "px";
-        }
-    }
+            obj.style.left = ev.pageX - disX + "px";
+            obj.style.top = ev.pageY - disY + "px";
+        };
+        document.onmouseup = function(){
+            document.onmousemove = null;
+            document.onmouseup = null;
+        };
+    };
+    return false;
 }
