0321b7270a081a92ed7452e77291adb986ceac62
Richardsh <2223239184@qq.com>
2016-05-17 09:26:29 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -4,7 +4,8 @@ function(id){
     var obj = document.getElementById(id);
     var disX=0,
         disY=0;
-    obj.onmousedown = function(){
-        
+    obj.onmousedown = function(ev){
+        disX = ev.pageX - obj.offsetLeft;
+        disY = ev.pageY - obj.offsetTop;
     }
 }
