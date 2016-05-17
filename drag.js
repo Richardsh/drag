5db204c6a11c38684aa1c6dd1f1b19f2e263ab39
Richardsh <2223239184@qq.com>
2016-05-17 09:58:21 +0800

    change drag.js from master

--- drag.js
+++ drag.js
@@ -7,5 +7,9 @@ function(id){
     obj.onmousedown = function(ev){
         disX = ev.pageX - obj.offsetLeft;
         disY = ev.pageY - obj.offsetTop;
+        document.onmousemove =function(){
+            obj.style.left = ev.pageX - ev.disX + "px";
+            obj.style.top = ev.pageY - ev.disY + "px";
+        }
     }
 }
